c12911ff174ba5aa1d5322e6104d2fed62a7393e
Jauhar40 <jauhar.ilmi@gmail.com>
2024-02-29 23:31:35 +0700

    commit tugas

---
+++ features/step-definitions/steps.js
@@ -0,0 +1,21 @@
+const { Given, When, Then } = require ('@wdio/cucumber-framework');
+
+const LoginPage = require ('../pageobjects/login.page.js');
+const HomePage = require ('../pageobjects/home.page.js');
+
+const pages = {
+    login: LoginPage
+}
+
+Given(/^I am on the (\w+) page$/, async (page) => {
+    await LoginPage.open()
+});
+
+When(/^I login with (\w+) and (.+)$/, async (username, password) => {
+    await LoginPage.login(username, password)
+});
+
+Then(/^I should see a flash message saying (.*)$/, async (message) => {
+    await HomePage.validateHomePage;
+});
+
